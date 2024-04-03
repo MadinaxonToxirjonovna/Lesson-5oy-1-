5086e5a7fb49e30b99602bb689350857abeb9c8d
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-04-03 23:33:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,118 @@
+// let year = +prompt("Yil kiriting")
+// console.log(typeof year);
+
+// if (year % 4 === 0) {
+//     alert("Bu yil 4 yilda bir keladigan yil");
+// } else {
+//     alert("Bu yi 4 yilda bir keladigan yil emas");
+// }
+
+
+// const array = ["👩‍💻", "🥊", "🍲" ];
+// alert(array);
+// const b = 0;
+
+// function myFunction(params) {
+
+// }
+
+
+// DOM mavzusi
+// let h1 = document.getElementById("h1")
+// let input = document.getElementById("input")
+// h1.innerHTML = "Hello World"
+
+
+
+
+let day =+prompt("Birdan 7 gacha son kiriting");
+let dayName;
+
+switch (day) {
+    case 0:
+    dayName = "Sunday"
+
+    case 1:
+    dayName = "Monday"
+    break;
+
+    case 2:
+    dayName = "Tuesday"
+    break;
+
+    case 3:
+    dayName = "Wednesday"
+    break;
+
+    case 4:
+    dayName = "Thursday"
+    break;
+
+    case 5:
+    dayName = "Friday"
+    break;
+
+    case 6:
+    dayName = "Saturday"
+    break;
+    default:
+        dayName = "Bir haftada 7 kun bor"
+}
+
+alert(dayName);
+
+
+                                                    // SINF ISHI
+
+// let day = +prompt("Biron bir son kiriting");
+// let dayName;
+// let a;
+// let b;
+
+// switch (day) {
+//     case 0:
+//         arrayName = ["Madina", "Xusniya", "Nasiba", "Abdulloh", "Yusuf"];
+//         for (const name of arrayName) {
+//             console.log(name);
+//         }
+
+//     case 1:
+//         for (let i = 0; i < 10; i++) {
+//             console.log("MARS IT SCHOOL");
+//         }
+//         break;
+
+//     case 2:
+//         dayName = (Math.pow(2, 3) + Math.pow(3, 2)) / 3;
+//         console.log(dayName);
+//         break;
+
+//     case 3:
+//         dayName = Math.floor(Math.abs(a) + Math.abs(b));
+//         console.log(dayName);
+//         break;
+
+//     case 4:
+//         dayName = Math.pow(9, 2);
+//         console.log(dayName);
+//         break;
+
+//     case 5:
+//         dayName = a * b;
+//         console.log(dayName);
+//         break;
+
+//     case 6:
+//         dayName = Math.abs(Math.pow(6, 2) - Math.pow(2, 2));
+//         console.log(dayName);
+//         break;
+
+//     case 7:
+//         dayName = Math.floor(Math.sqrt(2 * 2));
+//         console.log(dayName);
+//         break;
+//     default:
+
+// }
+
+// alert(dayName);
